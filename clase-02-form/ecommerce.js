8b3dd22e5e678e99eb8dd4ac76649787bcf198ac
Berni <pbernal@mendoza.edu.ar>
2025-11-07 21:04:20 -0300

    Creamos constantes

---
+++ clase-02-form/ecommerce.js
@@ -0,0 +1,9 @@
+
+
+// Varaibales
+let carrito;
+let totalCompra;
+let metodoPago;
+const IVA = 0.21;
+let usuario;
+let direccionEnvio;
--- clase-02-form/index.js
+++ clase-02-form/index.js
@@ -47,3 +47,63 @@ var marcaAuto = "Toyota", modeloAuto = "Corolla", anioAuto = 2020;
 var fruta, color, sabor; 
 
 console.log(fruta); //undefined porque no tiene valor asignado aún
+
+//prompt() // node no ejecuta comandos del front
+
+// Crear variables con la siguiente sintaxis
+var localidad; // minuscula
+var CUENTA_BANCARIA; //snake case (mayúsculas y guiones bajos)
+var paisDeOrigen; //camel case
+var $Estado; //con signo de dólar
+var _codigoPostal; //con guion bajo
+
+//var 1erNombre; //incorrecto, no puede comenzar con número
+//var nombre completo; //incorrecto, no puede tener espacios
+//var var; //incorrecto, "var" es una palabra reservada
+//var if; //incorrecto, "if" es una palabra reservada
+
+console.log(soltero, tieneMascota);
+
+// Constantes: son variables cuyo valor no puede cambiar
+// Cuando se declara una constante, se debe asignar un valor inmediatamente
+const IVA = 0.21; //declaración y asignación de constante
+const Brasil = "País limitrofe de Argentina";
+
+// Unimos las vaariables con texto para imprimir en consola
+console.log("El valor del IVA es: " + IVA); //concatenación de texto
+console.log("Un país limitrofe de Argentina es: " + Brasil);
+
+
+// Sumas
+let numeroUno = 10;
+let numeroDos = 20;
+let suma;
+
+suma = numeroUno + numeroDos; //operador matemático de suma
+
+console.log("La suma de " + numeroUno + " + " + numeroDos + " es igual a: " + suma);
+
+console.log(
+  "Texto plano que quiero mostrar: " 
+  + 
+  Brasil
+  +
+  " tiene una relación comercial con "
+  + 
+  pais
+  +
+  "."
+);
+
+console.log(
+  Brasil + ' , ' + 
+  'no es campeon del mundo'
+);
+
+numeroDos = 50; //reasignación de valor
+
+console.log(numeroDos);
+
+//IVA = 0.10; //error: no se puede reasignar una constante
+//final int DNI = 12315644
+
