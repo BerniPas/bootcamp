63e3b9eab774bb03854fc8f8a1e0292b0f73b107
Berni <pbernal@mendoza.edu.ar>
2026-01-07 21:29:26 -0300

    Confuguración de SASS con Node lista y funcionando

---
+++ clase-05-nodesass/index.js
@@ -0,0 +1,20 @@
+
+/* 1. Importamos la librería de express */
+const express = require('express');
+
+/* 2. Importamos path para que Node encuentre las rutas */
+const path = require('node:path');
+
+/* 3. Inicializamos la aplicación de express */
+const app = express();
+
+/* 4. Definimos el puerto */
+const PORT = 3000;
+
+/* 5. Definimos la carpeta pública */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* 6. Ejecutamos la aplicación */
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
