83109198da78c8dcac6cb1f7e727eff039371b0f
Berni <pbernal@mendoza.edu.ar>
2026-02-25 21:29:17 -0300

    Funciones para conexión a la base de datos de Mongo

---
+++ clase-08-miecommerce/src/database/conexionAtlas.js
@@ -0,0 +1,37 @@
+
+// 1. Importar mongoose para obtener la función de conexión a MongoDB
+const mongoose = require('mongoose');
+
+// 2. Importar dotenv para cargar las variables de entorno
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+// 3. Obtener la contraseña de MongoDB desde las variables de entorno
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+
+// 4. Construir la URI de conexión a MongoDB utilizando la contraseña
+const uri = `mongodb+srv://pastorbernal:${MONGO_PASSWORD}@cluster0.btrbimd.mongodb.net/?appName=Cluster0`;
+
+const clientOptions = { 
+  serverApi: 
+  { version: '1', 
+    strict: true, 
+    deprecationErrors: true 
+  } 
+};
+
+// 5. Crear una función asíncrona para establecer la conexión a MongoDB
+async function conexionMongo() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+module.exports = conexionMongo;
+
